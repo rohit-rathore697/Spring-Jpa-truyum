89349e925e6a52051781444ae33990d563cc3aa3
Rohit Singh Rathore <rohitsingh.rathore@cognizant.com>
2020-04-30 16:02:38 +0530

    Create MenuItem.java

---
+++ src/main/java/com/cognizant/truyum/model/MenuItem.java
@@ -0,0 +1,126 @@
+package com.cognizant.truyum.model;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "menuitem")
+public class MenuItem {
+	@Id
+	@Column(name = "m_id")
+	private long id;
+	@Column(name = "m_name")
+	private String name;
+	@Column(name = "m_price")
+	private float price;
+	@Column(name = "m_active_status")
+	private boolean active;
+	@Column(name = "m_dateoflaunch")
+	private java.util.Date dateOfLaunch;
+	@Column(name = "m_category")
+	private String category;
+	@Column(name = "m_freedelivery")
+	private boolean freeDelivery;
+
+	public MenuItem(long id, String name, float price, boolean active, java.util.Date dateOfLaunch, String category,
+			boolean freeDelivery) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.active = active;
+		this.dateOfLaunch = dateOfLaunch;
+		this.category = category;
+		this.freeDelivery = freeDelivery;
+	}
+
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public java.util.Date getDateOfLaunch() {
+		return dateOfLaunch;
+	}
+
+	public void setDateOfLaunch(Date dateOfLaunch) {
+		this.dateOfLaunch = dateOfLaunch;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public boolean isFreeDelivery() {
+		return freeDelivery;
+	}
+
+	public void setFreeDelivery(boolean freeDelivery) {
+		this.freeDelivery = freeDelivery;
+	}
+
+	@Override
+	public String toString() {
+		return "MenuItem [id=" + id + ", name=" + name + ", price=" + price + ", active=" + active + ", dateOfLaunch="
+				+ dateOfLaunch + ", category=" + category + ", freeDelivery=" + freeDelivery + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MenuItem other = (MenuItem) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+}
